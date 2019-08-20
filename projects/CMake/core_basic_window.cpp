4e72f41c292afa5db893b382fe34d53104aaaa0c
Rob Loach <robloach@gmail.com>
2019-08-19 21:29:02 -0400

    Add Project example

---
+++ projects/CMake/core_basic_window.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib/raylib.hpp"
+
+int main()
+{
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	int screenWidth = 800;
+	int screenHeight = 450;
+	raylib::Color background(RAYWHITE);
+	raylib::Color textColor(LIGHTGRAY);
+	raylib::Window w(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+	SetTargetFPS(60);
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!w.ShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		// TODO: Update your variables here
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		background.ClearBackground();
+
+		textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	return 0;
+}
