c2c1eb4e30e8405ad6e2acbefba7babbce26c0dd
Rob Loach <robloach@gmail.com>
2020-03-19 14:49:19 -0400

    Add a static library

---
+++ lib/libraylib-cpp.cpp
@@ -0,0 +1,11 @@
+/*******************************************************************************************
+*
+*   libraylib-cpp [raylib-cpp] library
+*
+*   This is a static C++ built library to make linking easier.
+*
+*   Copyright (c) 2015 Rob Loach (@RobLoach)
+*
+********************************************************************************************/
+
+#include "raylib/raylib.hpp"
