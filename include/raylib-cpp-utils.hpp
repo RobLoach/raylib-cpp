cc8b01a8d420205c34ec4934e4558d92223513a8
furudbat <hircreacc@gmail.com>
2024-03-02 01:29:19 +0100

    Add FileData and FileText

--- include/raylib-cpp-utils.hpp
+++ include/raylib-cpp-utils.hpp
@@ -19,4 +19,17 @@
     void Set##method(type value) { name = value; }
 #endif
 
+#ifndef GETTER
+/**
+ * A utility to build get methods on top of a property.
+ *
+ * @param type The type of the property.
+ * @param method The human-readable name for the method.
+ * @param name The machine-readable name of the property.
+ */
+#define GETTER(type, method, name) \
+    /** Retrieves the name value for the object. @return The name value of the object. */ \
+    type Get##method() const { return name; }
+#endif
+
 #endif  // RAYLIB_CPP_INCLUDE_RAYLIB_CPP_UTILS_HPP_
