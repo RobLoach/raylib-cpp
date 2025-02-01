cef0dfefa8c07a13ec945d84d074c3efa1af65c3
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2025-02-01 17:14:46 +0100

    fix memory leak in FileData::Load

--- include/FileData.hpp
+++ include/FileData.hpp
@@ -31,7 +31,10 @@ public:
     GETTER(int, BytesRead, bytesRead)
 
     void Load(const std::string& fileName) { Load(fileName.c_str()); }
-    void Load(const char* fileName) { data = ::LoadFileData(fileName, &bytesRead); }
+    void Load(const char* fileName) {
+        Unload();
+        data = ::LoadFileData(fileName, &bytesRead);
+    }
 
     void Unload() {
         if (data != nullptr) {
